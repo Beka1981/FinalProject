743878072dc64edad39cbc1d497aef29b5a92102
Beka <beka@gogichaishvili.ge>
2024-01-17 11:19:35 +0400

    Initial Commit

---
+++ FinalApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinalApplication
+//
+//  Created by Admin on 17.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
